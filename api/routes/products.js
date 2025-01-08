23584dbde6f882a42f1d5685cfa3a99a8887df43
seetaramoruganti <orugantiseetaram@gmail.com>
2025-01-08 11:23:12 -0500

    updated patch and delete api

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,45 +1,97 @@
 const express = require("express");
 
 const Product = require("../models/product");
+const { default: mongoose } = require("mongoose");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Handling GET requests to /products ",
-  });
+  Product.find()
+    .exec()
+    .then((docs) => {
+      console.log(docs);
+      res.status(200).json(docs);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 router.post("/", (req, res, next) => {
-  const product = {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price,
-  };
-  res.status(201).json({
-    message: "Handling POST nrequests to /products",
   });
+  product
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "Handling POST requests to /products",
+        createdProduct: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
-  if (id === "special") {
-    res
-      .status(200)
-      .json({ message: "You have Discovered the special ID", id: id });
-  } else {
-    res.status(200).json({ message: "you passed as ID" });
+  Product.findById(id)
+    .exec()
+    .then((doc) => {
+      console.log("From Database", doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res
+          .status(404)
+          .json({ message: " Not a valid entry found for the id input " });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(200).json({ error: err });
+    });
+});
+
+router.patch("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
   }
+  Product.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
-router.patch("/special", (req, res, next) => {
-  res.status(200).json({
-    message: "Updated Product !",
-  });
-});
-
-router.delete("/special", (req, res, next) => {
-  res.status(200).json({
-    message: "Deleted Product !",
-  });
+router.delete("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  Product.findByIdAndDelete(id)
+    .exec()
+    .then((result) => {
+      res.status(200).json({ result });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-const express = require("express");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const app = express();
-
-mongoose.connect(
-  "mongodb+srv://orugantiseetaram:" +
-    process.env.MONGO_ATLAS_PW +
-    "@node-rest-shop.ve5up.mongodb.net/?retryWrites=true&w=majority&appName=node-rest-shop",
-
-  {
-    useMongoClient: true,
-  }
-);
-
 // morgan will call next function api routes to log and doesn't
 //  return anything and node.js continues to work on next part
 
-app.use(morgan("dev")); // can be called middleware
-app.use(bodyParser.urlencoded({ extended: false })); // can be called middleware
-app.use(bodyParser.json());
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://orugantiseetaram:" +
+      process.env.MONGO_ATLAS_PW +
+      "@node-rest-shop.ve5up.mongodb.net/node-rest-shop?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB connected successfully!"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const productRoutes = require("./api/routes/products");
-
 const orderRoutes = require("./api/routes/orders");
 
+app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -32,21 +31,21 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type,Accept, Authorization"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, PATCH, DELETE, GET, POST");
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
     return res.status(200).json({});
   }
   next();
 });
 
-// routes that we are handling
+// Routes which should handle requests
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 
 app.use((req, res, next) => {
-  const error = new Error("Not found !");
+  const error = new Error("Not found");
   error.status = 404;
   next(error);
 });
@@ -54,7 +53,9 @@ app.use((req, res, next) => {
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
   res.json({
-    error: error.message,
+    error: {
+      message: error.message,
+    },
   });
 });
 
