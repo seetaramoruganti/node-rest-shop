01391bb3f3784c74e7126469b15aff30752cd51a
seetaramoruganti <orugantiseetaram@gmail.com>
2025-01-08 07:13:13 -0500

    added logging using morgan package

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
-
+const morgan = require('morgan');
 const app = express();
+
+// morgan will call next function api routes to log and doesn't
+//  return anything and node.js continues to work on next part 
+
+
+app.use(morgan('dev'));
 
 const productRoutes = require('./api/routes/products');
 
