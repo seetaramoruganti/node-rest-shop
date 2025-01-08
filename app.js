504624810fefba791bf289771e02c6c78b288eb5
seetaramoruganti <orugantiseetaram@gmail.com>
2025-01-08 08:32:35 -0500

    added CORS policies in app.js

--- app.js
+++ app.js
@@ -15,9 +15,27 @@ const productRoutes = require('./api/routes/products');
 
 const orderRoutes = require('./api/routes/orders');
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type,Accept, Authorization"
+    );
+    if(req.method === "OPTIONS"){
+        res.header('Access-Control-Allow-Methods', "PUT, PATCH, DELETE, GET, POST");
+        return res.status(200).json({});
+    }
+})
+
+
 // routes that we are handling 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+
 
 app.use((req, res, next )=>{
     const error = new Error('Not found !');
