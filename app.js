103d96800a8f6b6f88c32aa51ee2e5779e987ac5
seetaramoruganti <orugantiseetaram@gmail.com>
2025-01-08 07:07:16 -0500

    added script at global level for nodemon

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const productRoutes = require('./api/routes/products');
 
 const orderRoutes = require('./api/routes/orders');
 
+// routes that we are handling 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
