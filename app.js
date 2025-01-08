f89776aeba6220b8bf62ebe9b41d2b5d19edbe49
seetaramoruganti <orugantiseetaram@gmail.com>
2025-01-08 08:33:58 -0500

    updated CORS

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use((req, res, next)=>{
         res.header('Access-Control-Allow-Methods', "PUT, PATCH, DELETE, GET, POST");
         return res.status(200).json({});
     }
+    next();
 })
 
 
