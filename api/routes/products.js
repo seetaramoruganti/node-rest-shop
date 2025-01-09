62862288b446573d248e5b2143f3252e73cfdfcf
seetaramoruganti <orugantiseetaram@gmail.com>
2025-01-09 07:22:48 -0500

    added extra information of response and manipulated responses

--- api/routes/products.js
+++ api/routes/products.js
@@ -7,10 +7,24 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   Product.find()
+    .select("name price _id")
     .exec()
     .then((docs) => {
-      console.log(docs);
-      res.status(200).json(docs);
+      const response = {
+        count: docs.length,
+        products: docs.map((doc) => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            _id: doc._id,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/products/" + doc._id,
+            },
+          };
+        }),
+      };
+      res.status(200).json(response);
     })
     .catch((err) => {
       console.log(err);
