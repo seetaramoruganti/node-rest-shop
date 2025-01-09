c4d2ea40c2d2d89145c5a801191c1901be90b6da
seetaramoruganti <orugantiseetaram@gmail.com>
2025-01-09 07:13:42 -0500

    added constraints

---
+++ api/models/order.js
@@ -0,0 +1,3 @@
+const mongoose = require("mongoose");
+
+const Order = mongoose.Schema({});
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,34 +1,29 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res, next)=>{
-    res.status(200).json({message:"Orders were Fetched !"});
+router.get("/", (req, res, next) => {
+  res.status(200).json({ message: "Orders were Fetched !" });
 });
 
-
-router.post('/', (req, res, next)=>{
-    const order = {
-        productId:req.body.productId,
-        quantity: req.body.quantity
-
-
-    }
-    res.status(201).json({message:"Orders Created !", order:order});
+router.post("/", (req, res, next) => {
+  const order = {
+    productId: req.body.productId,
+    quantity: req.body.quantity,
+  };
+  res.status(201).json({ message: "Orders Created !", order: order });
 });
 
-
-router.get('/:orderId', (req, res, next)=>{
-    res.status(201).json({message:"Orders Details !",
-        orderId: req.params.orderId
-    });
+router.get("/:orderId", (req, res, next) => {
+  res
+    .status(201)
+    .json({ message: "Orders Details !", orderId: req.params.orderId });
 });
 
-
-router.delete('/:orderId', (req, res, next)=>{
-    res.status(201).json({message:"Orders Deleted !",
-        orderId: req.params.orderId
-    });
+router.delete("/:orderId", (req, res, next) => {
+  res
+    .status(201)
+    .json({ message: "Orders Deleted !", orderId: req.params.orderId });
 });
 
 module.exports = router;
